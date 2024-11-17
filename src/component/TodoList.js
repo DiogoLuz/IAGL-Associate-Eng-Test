27b1b03ffe3303087a28288ed455a95d4cbfdf40
Diogo Luz <emeraldosgaming@gmail.com>
2024-11-17 22:29:03 +0000

    feat(TodoList): added EnterToDo component to larger TodoList component and added wrapping parent div

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Todo from "./Todo";
 import { fetchTodos } from "../actions";
 import { connect } from "react-redux";
+import EnterToDo from "./EnterToDo";
 
 class TodoList extends Component {
   state = {};
@@ -13,13 +14,16 @@ class TodoList extends Component {
   render() {
     const { todos } = this.props.data;
     return (
-      <ul className="todo-list">
-        {todos && todos.length
-          ? todos.map((todo, index) => {
-              return <Todo key={`todo-${index}`} todo={todo.task} />;
-            })
-          : "No todos, yay!"}
-      </ul>
+      <div>
+        <ul className="todo-list">
+          {todos && todos.length
+            ? todos.map((todo, index) => {
+                return <Todo key={`todo-${index}`} todo={todo.task} />;
+              })
+            : "No todos, yay!"}
+        </ul>
+        <EnterToDo />
+      </div>
     );
   }
 }

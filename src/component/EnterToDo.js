6eb2cb8370245515acce002d51e9f49d3ccfbf1f
Diogo Luz <emeraldosgaming@gmail.com>
2024-11-17 22:27:34 +0000

    feat(EnterToDo): create an MVP of a form component that takes in to-do list item form entries

--- src/component/EnterToDo.js
+++ src/component/EnterToDo.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function EnterToDo() {
-  return <div>EnterToDo</div>;
+  return (
+    <form
+      method="post"
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+      }}
+    >
+      <label>
+        Enter To-Do List Item: <input name="todoListItem"></input>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default EnterToDo;

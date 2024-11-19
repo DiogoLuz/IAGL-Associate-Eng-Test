ee81cd2257b407f9f65cd7866f3c0a969099b6c5
Diogo Luz <emeraldosgaming@gmail.com>
2024-11-19 08:29:44 +0000

    feat: updated redux backend to allow adding of new to-dos

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,38 @@
 import axios from "axios";
-import { FETCH_TODOS } from "./types";
+import { FETCH_TODOS, ADD_TODO } from "./types";
 
 export function fetchTodos() {
-  return function(dispatch) {
+  return function (dispatch) {
     return axios.get("http://localhost:9091/api/todo").then(({ data }) => {
       dispatch(setTodos(data));
     });
   };
 }
 
+// Action to add a new todo
+export function addTodo(todo) {
+  return function (dispatch) {
+    return axios
+      .post("http://localhost:9091/api/todo", todo)
+      .then(({ data }) => {
+        // Dispatch an action to update the store with the new todo
+        dispatch({
+          type: ADD_TODO,
+          payload: data,
+        });
+        // Refetch the list of todos to ensure synchronization
+        dispatch(fetchTodos());
+      })
+      .catch((error) => {
+        console.error("Error adding todo:", error);
+        // Handle error cases if needed
+      });
+  };
+}
+
 function setTodos(data) {
   return {
     type: FETCH_TODOS,
-    payload: data
+    payload: data,
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
-import { FETCH_TODOS } from "../actions/types";
+import { ADD_TODO, FETCH_TODOS } from "../actions/types";
 
-export default function(state = {}, action) {
+export default function (state = { todo: [] }, action) {
   switch (action.type) {
     case FETCH_TODOS:
-      return { data: action.payload };
+      return { ...state, data: action.payload };
+
+    case ADD_TODO:
+      return { ...state, data: [...state.todo, action.payload] };
     default:
       return state;
   }
